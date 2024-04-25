48889df1003e0d9592643eb7329f766229f640e5
hakun0 <brithon.saniko@gmail.com>
2024-04-25 14:20:19 +0200

    annotation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# 1-concat.py
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
