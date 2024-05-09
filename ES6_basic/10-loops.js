f6b656f6cbea6cacc22b5c53dfb33314d76fca2f
hakun0 <brithon.saniko@gmail.com>
2024-05-09 17:15:12 +0200

    daccrr

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const final = [];
+    for (const value of array) {
+      final.push(appendString + value);
+    }
+    return final;
+  }
